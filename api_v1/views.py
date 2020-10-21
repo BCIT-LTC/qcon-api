7b4e122799f3a12198d692385d80241f98f043a4
Viresh Soedhwa <vsoedhwa@bcit.ca>
2020-10-21 13:57:51 -0700

    added root url for testing

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+import logging
+
+logger = logging.getLogger(__name__)
+
+class TestEndpoint(APIView):
+    def post(self, request, format=None):
+
+        logger.warning("helloo thi is warning")
+
+        logger.error("hthisi is info")
+
+        return Response("test")
