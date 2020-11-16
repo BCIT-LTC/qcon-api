db65c894212726340df56153bfa2e109cdc6be44
Viresh Soedhwa <vsoedhwa@bcit.ca>
2020-11-15 19:24:50 -0800

    added python generation command for reference

--- antlr/build.sh
+++ antlr/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-FILE="Grammar"
+FILE="Qcon"
 ANTLR=$(echo $CLASSPATH | tr ':' '\n' | grep -m 1 "antlr-4.8-complete.jar")
 java -jar $ANTLR $FILE.g4
 javac $FILE*.java
 
-# grun Grammar prog -gui test.txt -tokens
+# grun Qcon qcon -gui test.txt -tokens
+
+# antlr4 -Dlanguage=Python3 -visitor Qcon.g4
