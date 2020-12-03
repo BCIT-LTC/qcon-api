59c4282cf882755bb63caec1e68bb812a36a4ff0
Viresh Soedhwa <vsoedhwa@bcit.ca>
2020-12-03 09:57:07 -0800

    installed extensions for resetting db at startup
    (cherry picked from commit 611f896919c20971a1134df9816cb1057900bddf)

--- qcon/settings.py
+++ qcon/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import sys
 from pathlib import Path
-from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 # Third-Party Apps
+    'django_extensions',
     'rest_framework',
     'rest_framework.authtoken',
     'django_q',
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'qcon.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database',
     }
 }
 
