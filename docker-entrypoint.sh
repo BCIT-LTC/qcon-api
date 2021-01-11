215aa9d50155ebfb705db5b0b73c458ee646a51d
Viresh Soedhwa <vsoedhwa@bcit.ca>
2021-01-11 10:31:08 -0800

    Comments for dev and prod context

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 set -e
 
+#PROD
 >&2 echo "create logging directory"
 if [ ! -d "log" ]; then
     mkdir log
     touch log/error.log
 fi
+
+
+#PROD
 >&2 echo "make Database migrations"
 python manage.py makemigrations api_v1
 echo "-------------------------------------------------------------------------------------------\n"
 
+#PROD
 >&2 echo "Run Database migrations"
 python manage.py migrate
 echo "-------------------------------------------------------------------------------------------\n"
@@ -19,10 +24,12 @@ echo "--------------------------------------------------------------------------
 # nginx -g 'daemon off;'
 # nginx
 
+#PROD
 >&2 echo "Start Django Q task scheduler"
 python manage.py qcluster &
 echo "-------------------------------------------------------------------------------------------\n"
 
+#DEV
 >&2 echo "Create temporary superuser"
 echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python /code/manage.py shell
 echo "-------------------------------------------------------------------------------------------\n"
