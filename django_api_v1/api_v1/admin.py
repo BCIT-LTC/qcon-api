e189973c9cdd3fb829d19c9a21532fbe648d3195
Viresh Soedhwa <vsoedhwa@bcit.ca>
2021-01-22 15:10:58 -0800

    removed django package

--- django_api_v1/api_v1/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import QuestionLibrary, Question, Answer
-
-admin.site.register(QuestionLibrary)
-admin.site.register(Question)
-admin.site.register(Answer)
