ccfeeb854002a6dca19986c53e6421da2d69151c
Viresh Soedhwa <vsoedhwa@bcit.ca>
2021-01-22 19:07:29 -0800

    created helper functions

--- api_v1/tasks2.py
+++ api_v1/tasks2.py
@@ -27,6 +27,9 @@ import time
 
 from api_v1.L1Element import L1Element
 
+# =============================================================================================
+# =======================================L1 MAIN===============================================
+# =============================================================================================
 def parse_questions(question_library) :
     input = InputStream(question_library.pandoc_string)
     lexer = L1Lexer(input)
@@ -43,25 +46,18 @@ def parse_questions(question_library) :
     print("L1 done")
 
     # Populate L1 
-
-
     # Normalize array and grab indentations
-    
-    listofL1Elements = []    
-    for element in parsed_questions:
-        # print(element)
-        
-        L1 = L1Element()
 
-        if element['listseparator'] == False:
+    listofL1Elements = []    
+    for element in parsed_questions:        
+        L1 = L1Element()
+        if element['listitem'] == True:
             indent_length, normalized_prefix = normalize_prefix_and_grab_indent(element['prefix'])
             L1.prefix = normalized_prefix
-            L1.indentlength = indent_length
-  
+            L1.indentlength = indent_length  
         L1.content = element['content']
         L1.starmarked = element['correctprefix']
-        L1.listseparator = element['listseparator']
-
+        L1.listitem = element['listitem']
         listofL1Elements.append(L1)
 
     # for element in listofL1Elements:
@@ -74,21 +70,10 @@ def parse_questions(question_library) :
 
     # Split questions
     
-    # questions_separated = []
-    # if(listofL1Elements[0].prefix == "1"):            
-        # print("baselevel: " + str(listofL1Elements[0].indentlength))
-        # questions_separated.append(listofL1Elements[0])
-        # questions_separated = apply_question_separator(1, listofL1Elements, 1, questions_separated, listofL1Elements[0].indentlength) # questionindex, content, mainindex, baselevel
-        
     questions_separated = question_separate(listofL1Elements, 0, 0)
-    # for element in questions_separated:
-    #     print(element)
-    # print(str(questions_separated))
 
     # for element in questions_separated:
     #     print(element)
-
-
 
     # Split AnswerBlock by marking beginning of it
     
@@ -108,27 +93,28 @@ def parse_questions(question_library) :
         # if questions_separated[i] == '##########END_QUESTION##########':
         #     ending_found = True
 
-    for line in questions_separated:
-        print(line)
+    # for line in questions_separated:
+    #     print(line)
 
     # compile array or strings 
     collectionofstrings = []
     for i in range(0, len(questions_separated), 1):
-
-        # if i != 0:    
+        
         if questions_separated[i].questionseparator:
             collectionofstrings.append('##########START_QUESTION########\n')
 
         if questions_separated[i].answerblockseparator:
             collectionofstrings.append('##########START_ANSWER##########\n')
-        # print(questions_separated[i].prefix) 
-        if questions_separated[i].listseparator is not True:
-            collectionofstrings.append(questions_separated[i].prefix + "." + questions_separated[i].content)
-        # collectionofstrings.append(questions_separated[i].prefix)
-    # print(collectionofstrings)
-
-
-    # TODO Normalize content to fix bold or other stylings
+        
+        # THIS IS FOR LIST ITEMS
+        if questions_separated[i].listitem:
+            if questions_separated[i].starmarked:
+                collectionofstrings.append(questions_separated[i].prefix + "*." + questions_separated[i].content)
+            else:
+                collectionofstrings.append(questions_separated[i].prefix + "." + questions_separated[i].content)
+        # THIS IS FOR REGULAR CONTENT (seperators, feedback markers, questionheader markers etc etc)
+        else:
+            collectionofstrings.append(questions_separated[i].content)
 
 
     thestring = ''
@@ -137,26 +123,11 @@ def parse_questions(question_library) :
     print(thestring)
     # TODO: pass the split questions to the L2 grammar
 
-
     return None
 
-def normalize_prefix_and_grab_indent(prefix):
-    # TODO: function to clean up extra characters on the prefix
-    # print("normalize_prefix: " + str(prefix))
-    # x = re.findall("([a-zA-Z0-9]+)", prefix)
-    # print(x[0])
-    x = re.findall("^(\\n)(>?[ ]*)([a-zA-Z0-9]+)", prefix)
-    # print("indent length " + str(len(x[0][1])))
-    # print("normalized prefix " + str(x[0][2]))
-    indent_length = len(x[0][1])
-    normalized_prefix = str(x[0][2])
-
-    return indent_length,normalized_prefix
-
-def normalize_content(content):
-    # TODO: function to clean up extra characters on the content
-    return content
-
+# =============================================================================================
+# ===============================Question Splitter=============================================
+# =============================================================================================
 
 def question_separate(data,index,question):
     # print("++++++++++++++")
@@ -225,6 +196,10 @@ def question_separate(data,index,question):
     else:
         return question_separate(data, index+1, question)
 
+# =============================================================================================
+# ===============================HELPER FUNCTIONS==============================================
+# =============================================================================================
+
 def find_index_of_next_number(data, startindex):
     index = int(startindex)+1
     while data[index].prefix.isnumeric() == False:
@@ -245,14 +220,30 @@ def find_index_of_previous_number(data, startindex):
     return index
 
 def check_fib(content):
-    x = re.search("\[.*,.*\]", content)
+    x = re.search("[\[.*,.*\]]", content)
     if x:
         return True
     else:
         return False
-# def backtrack_analyze_previous(data,start,stop)
 
+def normalize_prefix_and_grab_indent(prefix):
+    # TODO: function to clean up extra characters on the prefix
+    x = re.findall("^(\\n)(>?[ ]*)([a-zA-Z0-9]+)", prefix)
+    # print("indent length " + str(len(x[0][1])))
+    # print("normalized prefix " + str(x[0][2]))
+    indent_length = len(x[0][1])
+    normalized_prefix = str(x[0][2])
 
+    return indent_length,normalized_prefix
+
+def normalize_content(content):
+    # TODO: function to clean up extra characters on the content
+    return content
+
+def remove_prefix(text, prefix):
+    return re.sub(r'^{0}'.format(re.escape(prefix)), '', text)
+
+'''
 def apply_question_separator(questionindex, listofL1Elements, mainindex, questions_separated, baselevel):
 
     print("==========")
@@ -295,7 +286,6 @@ def apply_question_separator(questionindex, listofL1Elements, mainindex, questio
 
     if listofL1Elements[mainindex].prefix.isnumeric():
         print("nummber iss FOUNDD")
-        print("array boiis " + listofL1Elements[mainindex].prefix)
         print("sep befpre "+ str(listofL1Elements[mainindex-1].listseparator))
         if listofL1Elements[mainindex-1].listseparator:  # question seperator added before
             print("check previous for separator")
@@ -328,55 +318,7 @@ def apply_question_separator(questionindex, listofL1Elements, mainindex, questio
             print("letterrrrr")
             questions_separated.append(listofL1Elements[mainindex])
             return apply_question_separator(questionindex, listofL1Elements, mainindex+1, questions_separated, baselevel)
-
-        # if int(listofL1Elements[mainindex].indentlength) > baselevel: # indented
-        #     print("IS INDENTED")
-        #     questions_separated.append(listofL1Elements[mainindex])
-        #     apply_question_separator(questionindex, listofL1Elements, mainindex+1, questions_separated, baselevel)
-        # else:
-        #     print("IS NOT INDENTED")
-        #     questions_separated.append(listofL1Elements[mainindex])
-        #     apply_question_separator(questionindex, listofL1Elements, mainindex+1, questions_separated, baselevel)
-
-    # if listofL1Elements[mainindex].prefix.isnumeric():
-    #     print("IS NUMERIC")
-        # if int(listofL1Elements[mainindex].indentlength) > baselevel: # indented
-        #     print("IS INDENTED")
-        #     questions_separated.append(listofL1Elements[mainindex])
-        #     apply_question_separator(questionindex, listofL1Elements, mainindex+1, questions_separated, baselevel)
-        # else:
-        #     print("IS NOT INDENTED")
-        #     if int(listofL1Elements[mainindex].prefix) == int(questionindex+1): # not indented
-
-        #         L1 = L1Element()
-        #         L1.prefix='##########END_QUESTION##########'
-        #         questions_separated.append(L1) 
-        #         questions_separated.append(listofL1Elements[mainindex])               
-        #         apply_question_separator(questionindex + 1, listofL1Elements, mainindex+1, questions_separated, baselevel)
-        # if listofL1Elements[mainindex+1].prefix == int(questionindex+1):
-        #     L1 = L1Element()
-        #     L1.questionseparator = True
-        #     questions_separated.append(L1)
-        #     apply_question_separator(questionindex+1, listofL1Elements, mainindex+1, questions_separated, baselevel)
-
-    # else:
-    #     if listofL1Elements[mainindex].listseparator:
-    #         if listofL1Elements[mainindex+1].prefix == int(questionindex+1):
-    #             L1 = L1Element()
-    #             L1.questionseparator = True
-    #             questions_separated.append(L1)
-    #             apply_question_separator(questionindex+1, listofL1Elements, mainindex+1, questions_separated, baselevel)
-    #         else:
-    #             apply_question_separator(questionindex, listofL1Elements, mainindex+1, questions_separated, baselevel)
-    #     else:
-    #         apply_question_separator(questionindex, listofL1Elements, mainindex+1, questions_separated, baselevel)
-
-
-    # L1 = L1Element()
-    # # L1.prefix='##########END_QUESTION##########'
-    # L1.prefix = listofL1Elements[mainindex].prefix
-    # L1.questionseparator = True
-    # questions_separated.append(L1) 
+'''
 
 
 def runconversion(question_library):
