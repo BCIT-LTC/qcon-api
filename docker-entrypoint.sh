59c8cca8358ad8801adb63b287fbc0d231c6deeb
Viresh Soedhwa <vsoedhwa@bcit.ca>
2021-03-17 16:39:59 -0700

    created admin credential env

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -19,7 +19,7 @@ echo "--------------------------------------------------------------------------
 # echo "-------------------------------------------------------------------------------------------\n"
 
 >&2 echo "Create temporary superuser"
-echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python /code/manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_CREDENTIAL')" | python /code/manage.py shell
 echo "-------------------------------------------------------------------------------------------\n"
 
 #Start django dev server
