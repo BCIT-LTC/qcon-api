809b205befaa7faf970efc37d76b9b6da534aba2
Viresh Soedhwa <vsoedhwa@bcit.ca>
2021-03-23 10:49:58 -0700

    Inject API key from deployment file as ENV

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,22 +18,35 @@ echo "--------------------------------------------------------------------------
 # python manage.py qcluster &
 # echo "-------------------------------------------------------------------------------------------\n"
 
->&2 echo "Create temporary superuser"
+>&2 echo "Create superuser 'admin'"
 echo "from django.contrib.auth.models import User; \
         User.objects.filter(username='admin').exists() or \
         User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_CREDENTIAL');" \
-| python /code/manage.py shell
+    | python /code/manage.py shell
 echo "-------------------------------------------------------------------------------------------\n"
 
 
->&2 echo "Create temporary API TOKEN"
-echo "from rest_framework.authtoken.models import Token; \
+# >&2 echo "Create temporary API TOKEN"
+# echo "from rest_framework.authtoken.models import Token; \
+#         from django.contrib.auth.models import User; \
+#         theuser = User.objects.get(username='admin'); \
+#         token = Token.objects.create(user=theuser); \
+#         print('API Token: ' + str(token))" \
+#     | python /code/manage.py shell
+# echo "-------------------------------------------------------------------------------------------\n"
+
+>&2 echo "Create API TOKEN"
+echo "from api_v2.models import CustomToken; \
         from django.contrib.auth.models import User; \
         theuser = User.objects.get(username='admin'); \
-        token = Token.objects.create(user=theuser); \
+        token = CustomToken.objects.create(user=theuser, key='$API_KEY'); \
         print('API Token: ' + str(token))" \
     | python /code/manage.py shell
 echo "-------------------------------------------------------------------------------------------\n"
+
+# >&2 echo "Create temporary API TOKEN"
+# python manage.py drf_create_token admin
+
 
 
 #Start django dev server
