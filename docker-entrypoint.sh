acb6fed47a2f8c493953d9e573e830722159e913
Viresh Soedhwa <vsoedhwa@bcit.ca>
2021-05-05 11:31:50 -0700

    allow admin username to be customized

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -50,32 +50,21 @@ fi
 
 >&2 echo "Create superuser 'admin'"
 echo "from django.contrib.auth.models import User; \
-        User.objects.filter(username='admin').exists() or \
-        User.objects.create_superuser('admin', 'admin@example.com', '$ADMIN_CREDENTIAL');" \
+        User.objects.filter(username='$ADMIN_USERNAME').exists() or \
+        User.objects.create_superuser('$ADMIN_USERNAME', 'admin@example.com', '$ADMIN_PASSWORD');" \
     | python /code/manage.py shell
 echo "-------------------------------------------------------------------------------------------\n"
-
-
-# >&2 echo "Create temporary API TOKEN"
-# echo "from rest_framework.authtoken.models import Token; \
-#         from django.contrib.auth.models import User; \
-#         theuser = User.objects.get(username='admin'); \
-#         token = Token.objects.create(user=theuser); \
-#         print('API Token: ' + str(token))" \
-#     | python /code/manage.py shell
-# echo "-------------------------------------------------------------------------------------------\n"
 
 >&2 echo "Create API TOKEN"
 echo "from api_v2.models import CustomToken; \
         from django.contrib.auth.models import User; \
-        theuser = User.objects.get(username='admin'); \
-        token = CustomToken.objects.create(user=theuser, key='$API_KEY'); \
-        print('API Token: ' + str(token))" \
+        theuser = User.objects.get(username='$ADMIN_USERNAME'); \
+        CustomToken.objects.filter(user=theuser).exists() or \
+        CustomToken.objects.create(user=theuser, key='$API_KEY'); \
+        print('API Token: ' + str(CustomToken.objects.get(user=theuser)))" \
     | python /code/manage.py shell
 echo "-------------------------------------------------------------------------------------------\n"
-
-# >&2 echo "Create temporary API TOKEN"
-# python manage.py drf_create_token admin
+        # print('API Token: ' + str(token))" \
 
 chmod -R 755 /var/lib/nginx
 
