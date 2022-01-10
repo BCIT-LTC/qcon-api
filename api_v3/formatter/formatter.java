34408eabfcd7ab4f1e36759af67f69170fc85df9
Viresh Soedhwa <vsoedhwa@bcit.ca>
2022-01-10 13:22:15 -0800

    fix: fix bug in the visitor function

--- api_v3/formatter/formatter.java
+++ api_v3/formatter/formatter.java
@@ -18,91 +18,98 @@ import java.util.Map;
 // import org.w3c.dom.Document;
 // import org.w3c.dom.Element;
 
-import java.io.File;  // Import the File class
-import java.io.IOException;  // Import this class to handle errors
+import java.io.File; // Import the File class
+import java.io.IOException; // Import this class to handle errors
 
 import java.nio.file.*;
 
 public class formatter {
 
    public static class formatterVisitor extends
-        formatterBaseVisitor<Void>
-    {
-        Map<String,String> props = new OrderedHashMap<String, String>();
-        int count = 0;
-      public Void visitRootheading(formatterParser.RootheadingContext ctx) {
-            props.put("rootheading",ctx.getText());
-            // props.put(id, value);
-            return null; // Java says must return something even when Void
-        }
-            props.put(Integer.toString(count),ctx.getText());
-            return null; // Java says must return something even when Void
-        }
-    }
+         formatterBaseVisitor<Void> {
+      Map<String, String> props = new OrderedHashMap<String, String>();
+      int count = 0;
 
+      public Void visitRootheading(formatterParser.RootheadingContext ctx) {
+         props.put("rootheading", ctx.getText());
+         // props.put(id, value);
+         return null; // Java says must return something even when Void
+      }
+
+      public Void visitSection(formatterParser.SectionContext ctx) {
+         count += 1;
+         // System.out.println(Integer.toString(count));
+         props.put(Integer.toString(count), ctx.getText());
+         // props.put(id, value);
+         return null; // Java says must return something even when Void
+      }
+   }
 
    public static void main(String args[]) {
       // System.out.println("Number of Command Line Argument = "+args.length);
-      // System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));		
-         // for(int i = 0; i< args.length; i++) {
-         //    System.out.println(String.format("Command Line Argument %d is %s", i, args[i]));
-         // }      
+      // System.out.println(String.format("Command Line Argument %d is %s", i,
+      // args[i]));
+      // for(int i = 0; i< args.length; i++) {
+      // System.out.println(String.format("Command Line Argument %d is %s", i,
+      // args[i]));
+      // }
 
-      if(args.length < 1) {
+      if (args.length < 1) {
          System.out.println("filename not provided");
          System.exit(0);
-      }	   
+      }
 
       // try{
-      //    DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();      
-      //    DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();      
-      //    Document document = documentBuilder.newDocument();
-      //    // root element
-      //    Element root = document.createElement("company");
-      //    document.appendChild(root);
-      //    // employee element
-      //    Element employee = document.createElement("employee");
-      //    root.appendChild(employee);
-      //    // set an attribute to staff element
-      //    Attr attr = document.createAttribute("id");
-      //    attr.setValue("10");
-      //    employee.setAttributeNode(attr);
-      //    // firstname element
-      //    Element firstName = document.createElement("firstname");
-      //    firstName.appendChild(document.createTextNode("James"));
-      //    employee.appendChild(firstName);
-      //    // lastname element
-      //    Element lastname = document.createElement("lastname");
-      //    lastname.appendChild(document.createTextNode("Harley"));
-      //    employee.appendChild(lastname);
-      //    // email element
-      //    Element email = document.createElement("email");
-      //    email.appendChild(document.createTextNode("james@example.org"));
-      //    employee.appendChild(email);
-      //    // department elements
-      //    Element department = document.createElement("department");
-      //    department.appendChild(document.createTextNode("Human Resources"));
-      //    employee.appendChild(department);
-      //    // create the xml file
-      //    //transform the DOM Object to an XML File
-      //    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      //    Transformer transformer = transformerFactory.newTransformer();
-      //    DOMSource domSource = new DOMSource(document);
-      //    StreamResult streamResult = new StreamResult(new File(args[0]));
-      //    transformer.transform(domSource, streamResult);
-      //    System.out.println("Done creating XML File");         
+      // DocumentBuilderFactory documentFactory =
+      // DocumentBuilderFactory.newInstance();
+      // DocumentBuilder documentBuilder = documentFactory.newDocumentBuilder();
+      // Document document = documentBuilder.newDocument();
+      // // root element
+      // Element root = document.createElement("company");
+      // document.appendChild(root);
+      // // employee element
+      // Element employee = document.createElement("employee");
+      // root.appendChild(employee);
+      // // set an attribute to staff element
+      // Attr attr = document.createAttribute("id");
+      // attr.setValue("10");
+      // employee.setAttributeNode(attr);
+      // // firstname element
+      // Element firstName = document.createElement("firstname");
+      // firstName.appendChild(document.createTextNode("James"));
+      // employee.appendChild(firstName);
+      // // lastname element
+      // Element lastname = document.createElement("lastname");
+      // lastname.appendChild(document.createTextNode("Harley"));
+      // employee.appendChild(lastname);
+      // // email element
+      // Element email = document.createElement("email");
+      // email.appendChild(document.createTextNode("james@example.org"));
+      // employee.appendChild(email);
+      // // department elements
+      // Element department = document.createElement("department");
+      // department.appendChild(document.createTextNode("Human Resources"));
+      // employee.appendChild(department);
+      // // create the xml file
+      // //transform the DOM Object to an XML File
+      // TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      // Transformer transformer = transformerFactory.newTransformer();
+      // DOMSource domSource = new DOMSource(document);
+      // StreamResult streamResult = new StreamResult(new File(args[0]));
+      // transformer.transform(domSource, streamResult);
+      // System.out.println("Done creating XML File");
       // }catch (ParserConfigurationException pce) {
-      //    System.out.println("An error occurred.");
-      //    pce.printStackTrace();
+      // System.out.println("An error occurred.");
+      // pce.printStackTrace();
       // }catch (TransformerException tfe) {
-      //    tfe.printStackTrace();
+      // tfe.printStackTrace();
       // }
 
       // ANTLR
       String javaClassContent = "";
       try {
-            Path fileName = Paths.get(args[0]);
-            javaClassContent = Files.readString(fileName);
+         Path fileName = Paths.get(args[0]);
+         javaClassContent = Files.readString(fileName);
       } catch (IOException e) {
          System.out.println("An error occurred.");
          e.printStackTrace();
@@ -122,16 +129,17 @@ public class formatter {
       System.out.println(loader.props);
 
       // try{
-      //    // create the xml file
-      //    //transform the DOM Object to an XML File
-      //    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      //    Transformer transformer = transformerFactory.newTransformer();
-      //    DOMSource domSource = new DOMSource(result);
-      //    StreamResult streamResult = new StreamResult(new File("./code/temp/test123"));
-      //    transformer.transform(domSource, streamResult);
-      //    System.out.println("Done creating XML File");
+      // // create the xml file
+      // //transform the DOM Object to an XML File
+      // TransformerFactory transformerFactory = TransformerFactory.newInstance();
+      // Transformer transformer = transformerFactory.newTransformer();
+      // DOMSource domSource = new DOMSource(result);
+      // StreamResult streamResult = new StreamResult(new
+      // File("./code/temp/test123"));
+      // transformer.transform(domSource, streamResult);
+      // System.out.println("Done creating XML File");
       // }catch (TransformerException tfe) {
-      //    tfe.printStackTrace();
+      // tfe.printStackTrace();
       // }
    }
 }
