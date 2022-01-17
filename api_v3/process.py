9edae75edd9438f031e4c9b5140be87cc7b89fed
Viresh Soedhwa <vsoedhwa@bcit.ca>
2022-01-17 13:27:39 -0800

    add subprocess to run terminal command

--- api_v3/process.py
+++ api_v3/process.py
@@ -1,18 +1,36 @@
 
 import formatter
-
+import subprocess
 
 def create_section_name():
     pass
 
 # Input markdown , Output Root elements(optional RootHeader, Mandatory Body and optional Answers)
 # No writing to model here
-def markdown_to_root_elements(questionlibrary):
+# def markdown_to_root_elements(questionlibrary):
 
-    from api_v3.formatter.formatter import formatter
+#     from api_v3.formatter.formatter import formatter
+#     formatter(questionlibrary)
 
-    formatter(questionlibrary)
+#     pass
 
+def run_formatter(questionlibrary):
+
+    process = subprocess.Popen(['pwd'], 
+                           stdout=subprocess.PIPE,
+                           universal_newlines=True)
+
+    while True:
+        output = process.stdout.readline()
+        print(output.strip())
+        # Do something else
+        return_code = process.poll()
+        if return_code is not None:
+            print('RETURN CODE', return_code)
+            # Process has finished, read rest of the output 
+            for output in process.stdout.readlines():
+                print(output.strip())
+            break
 
     pass
 
@@ -35,6 +53,6 @@ def process(questionlibrary):
 
     # ====================== Convert to markdown
     # print(questionlibrary.pandoc_string)
-    markdown_to_root_elements(questionlibrary)
+    run_formatter(questionlibrary)
 
     pass
