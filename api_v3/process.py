926a11984af3ca479dae2a66f2e15039e6d028e5
Viresh Soedhwa <vsoedhwa@bcit.ca>
2022-01-18 10:18:32 -0800

    fix: resolve issue with python path not correct

--- api_v3/process.py
+++ api_v3/process.py
@@ -1,9 +1,8 @@
-
-import formatter
-import subprocess
+import os
 
 def create_section_name():
     pass
+
 
 # Input markdown , Output Root elements(optional RootHeader, Mandatory Body and optional Answers)
 # No writing to model here
@@ -14,37 +13,29 @@ def create_section_name():
 
 #     pass
 
+
 def run_formatter(questionlibrary):
 
-    process = subprocess.Popen(['pwd'], 
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-
-    while True:
-        output = process.stdout.readline()
-        print(output.strip())
-        # Do something else
-        return_code = process.poll()
-        if return_code is not None:
-            print('RETURN CODE', return_code)
-            # Process has finished, read rest of the output 
-            for output in process.stdout.readlines():
-                print(output.strip())
-            break
+    os.chdir('/formatter/jarfile')
+    os.system('java -cp formatter.jar:* formatter')
+    os.chdir('/code')
 
     pass
+
 
 # Input Body , Output Array of 1 or more sections
 # No writing to model here
 def body_to_sections():
     pass
 
-# Input section, Output Array of 1 or more Questions 
+
+# Input section, Output Array of 1 or more Questions
 # No writing to model here
 def section_to_questions():
     pass
 
-# This function will most likely writes directly to model. Might need to move to model instead 
+
+# This function will most likely writes directly to model. Might need to move to model instead
 def run_parser():
     pass
 
@@ -54,5 +45,5 @@ def process(questionlibrary):
     # ====================== Convert to markdown
     # print(questionlibrary.pandoc_string)
     run_formatter(questionlibrary)
-
+    
     pass
