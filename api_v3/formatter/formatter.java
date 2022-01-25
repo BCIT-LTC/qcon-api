a536ca24c77bc17bd0b936a4199b4cf2f9af6b23
Viresh Soedhwa <vsoedhwa@bcit.ca>
2022-01-25 08:54:23 -0800

    removed section and endanswer rules

--- api_v3/formatter/formatter.java
+++ api_v3/formatter/formatter.java
@@ -30,65 +30,24 @@ public class formatter {
    public static Document document;
    public static Element root;
 
-   // public formatter() {
-   // String publictest = "haihfhf";
-   // }
    public static class formatterVisitor extends
          formatterBaseVisitor<Void> {
-      // Map<String, String> props = new OrderedHashMap<String, String>();
-
-      int count = 0;
-
-      public Void visitRootheader(formatterParser.RootheaderContext ctx) {
-         // props.put("rootheading", ctx.getText());
-         // props.put(id, value);
-         System.out.println("THIS IS HEADER");
+      public Void visitRootheader(formatterParser.RootheaderContext ctx){
          Element rootheader = document.createElement("rootheader");
          rootheader.appendChild(document.createTextNode(ctx.getText()));
          root.appendChild(rootheader);
-         return null; // Java says must return something even when Void
+         return null;
       }
 
-      public Void visitSection(formatterParser.SectionContext ctx) {
-         count += 1;
-         // System.out.println(Integer.toString(count));
-         // props.put(Integer.toString(count), ctx.getText());
-         // props.put(id, value);
-         System.out.println("SECTION FOUND");
-
-         Element Section = document.createElement("section");
-         Section.appendChild(document.createTextNode(ctx.getText()));
-         Attr attr = document.createAttribute("id");
-         attr.setValue(Integer.toString(count));
-         Section.setAttributeNode(attr);
-
-         root.appendChild(Section);
-
-         return null; // Java says must return something even when Void
-      }
-
-      public Void visitEnd_answers_block(formatterParser.End_answers_blockContext ctx) {
-         // System.out.println(Integer.toString(count));
-         // props.put(Integer.toString(count), ctx.getText());
-         // props.put(id, value);
-         System.out.println("End_answers_block FOUND");
-
-         Element End_answer = document.createElement("endanswer");
-         End_answer.appendChild(document.createTextNode(ctx.getText()));
-         root.appendChild(End_answer);
-
-         return null; // Java says must return something even when Void
+      public Void visitBody(formatterParser.BodyContext ctx){
+         Element body = document.createElement("body");
+         body.appendChild(document.createTextNode(ctx.getText()));
+         root.appendChild(body);
+         return null;
       }
    }
 
    public static void main(String args[]) {
-      // System.out.println("Number of Command Line Argument = "+args.length);
-      // System.out.println(String.format("Command Line Argument %d is %s", i,
-      // args[i]));
-      // for(int i = 0; i< args.length; i++) {
-      // System.out.println(String.format("Command Line Argument %d is %s", i,
-      // args[i]));
-      // }
 
       if (args.length < 1) {
          System.out.println("filename not provided");
@@ -97,23 +56,20 @@ public class formatter {
          System.out.println(args[0]);
       }
 
-      String javaClassContent = "";
+      String pandocContent = "";
       String inputfile = args[0] + "pandoc_string";
 
       try {
          Path fileName = Paths.get(inputfile);
-         javaClassContent = Files.readString(fileName);
+         pandocContent = Files.readString(fileName);
       } catch (IOException e) {
          System.out.println("An error occurred.");
          e.printStackTrace();
       }
-      // System.out.println(javaClassContent);
       System.out.println("starting parsing");
-      formatterLexer formatterLexer = new formatterLexer(CharStreams.fromString(javaClassContent));
+      formatterLexer formatterLexer = new formatterLexer(CharStreams.fromString(pandocContent));
       CommonTokenStream tokens = new CommonTokenStream(formatterLexer);
       formatterParser parser = new formatterParser(tokens);
-      // formatterParser.FormatterContext tree = parser.formatter();
-      // ParseTreeWalker walker = new ParseTreeWalker();
 
       ParseTree tree = parser.formatter();
 
@@ -144,6 +100,5 @@ public class formatter {
       } catch (TransformerException tfe) {
          tfe.printStackTrace();
       }
-      // System.out.println(loader.props);
    }
 }
