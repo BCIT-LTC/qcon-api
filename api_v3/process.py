0c2106bfcf01b9c36474442787644301b5576153
Viresh Soedhwa <vsoedhwa@bcit.ca>
2022-02-01 22:05:29 -0800

    add checks to ensure body and end_answer exists

--- api_v3/process.py
+++ api_v3/process.py
@@ -1,21 +1,46 @@
+from aifc import Error
 import os
 import xml.etree.ElementTree as ET
+import logging
 
+logger = logging.getLogger(__name__)
 
 def create_section_name():
     pass
 
 def run_formatter(questionlibrary):
 
-    os.chdir('/formatter/jarfile')
-    destination = '/code/temp/' + str(questionlibrary.id) + '/'
-    os.system('java -cp formatter.jar:* formatter ' + destination)
-    os.chdir('/code')
+    root = None
+    try:
+        os.chdir('/formatter/jarfile')
+        destination = '/code/temp/' + str(questionlibrary.id) + '/'
+        os.system('java -cp formatter.jar:* formatter ' + destination)
+        os.chdir('/code')
 
-    tree = ET.parse('/code/temp/' + str(questionlibrary.id) + '/' + 'formatter.xml')
-    root = tree.getroot()
-    for child in root:
-        print(child.tag, child.attrib)
+        tree = ET.parse('/code/temp/' + str(questionlibrary.id) + '/' + 'formatter.xml')
+        root = tree.getroot()
+    except:
+        pass
+
+    try:
+        if root[0].tag == "body" :
+            pass
+        else:
+            logger.error("Body not found")
+    except:
+        logger.error("Body not found")
+
+    try:
+        if root[1].tag == "end_answers" :
+            questionlibrary.end_answers = root[1].text
+            questionlibrary.save()
+        else:
+            logger.error("Answer Section not found")
+    except:
+        logger.error("Answer section not found")
+
+
+
 
 
 # Input Body , Output Array of 1 or more sections
