e43dd7a94efa8a583fe768a75a803e502f7a466a
Viresh Soedhwa <vsoedhwa@bcit.ca>
2022-03-24 14:35:55 -0700

    feat: save bytearray file to model

--- api_v3/consumers.py
+++ api_v3/consumers.py
@@ -1,7 +1,14 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from django.core.files.base import ContentFile
+
+from .models import QuestionLibrary
 
 class TextConsumer(AsyncWebsocketConsumer):
+
+    uploadedfile = None
+
     async def connect(self):
         print("connected")
         await self.accept()
@@ -22,8 +29,9 @@ class TextConsumer(AsyncWebsocketConsumer):
         # text_data_json = json.loads(text_data)
         # message = text_data_json['message']
 
-        print(bytes_data)
+        # print(bytes_data)
         # import socket
+        await database_sync_to_async(self.save_file)(bytes_data)
 
         import socket
         await self.send(text_data=json.dumps({
@@ -31,3 +39,16 @@ class TextConsumer(AsyncWebsocketConsumer):
             'data' : "data after file received by API",
             'progress' : "done"
         }))
+
+    def save_file(self, bytes_data):
+
+        received_file = ContentFile(bytes_data, name='foo.docx')
+
+        newfile = QuestionLibrary.objects.create()
+        newfile.temp_file = received_file
+        newfile.save()
+
+        # image = ImageFile(io.BytesIO(image_bytes), name='foo.jpg')  # << the answer!
+        # new_message = Message.objects.create(image=image)
+
+        return 
