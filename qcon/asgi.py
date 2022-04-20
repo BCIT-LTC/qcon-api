7f5f1d75abde3ac01be2a7d314b1441982bd43e1
Kyle Hunter <kyle_hunter@bcit.ca>
2022-04-20 11:19:43 -0700

    debug

--- qcon/asgi.py
+++ qcon/asgi.py
@@ -16,16 +16,16 @@ import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import api_v3.routing
+# import api_v3.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qcon.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            api_v3.routing.websocket_urlpatterns
-        )
-    ),
+    # "websocket": AuthMiddlewareStack(
+    #     URLRouter(
+    #         api_v3.routing.websocket_urlpatterns
+    #     )
+    # ),
     # Just HTTP for now. (We can add other protocols later.)
 })
