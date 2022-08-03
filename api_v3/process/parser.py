144ed6996514c823ff9dc56bea047e76ee98a32e
Arvin Rolos <arolos@bcit.ca>
2022-08-03 11:10:26 -0700

    fix: fib title, images, order, and html

--- api_v3/process/parser.py
+++ api_v3/process/parser.py
@@ -370,15 +370,15 @@ def parse_question(questionlibrary, question):
 
                 if x.start() == 0:
                     answer_at_start = True
-                
-                list_of_answers = re.findall(r"\\\[(.*?)\\\]", question_from_xml.text)
-                replaced_answers = re.sub(r"\\\[(.*?)\\\]", "_______", question_from_xml.text)
-
+                list_of_answers = re.findall(r"\[(.*?)\]", question.text)
+                replaced_answers = re.sub(r"\[(.*?)\]", "_______", question.text)
+                fib_title = re.sub(r"\\\[(.*?)\\\]", "_______", question_from_xml.text)
+                fib_title = re.sub('<!-- NewLine -->', '', fib_title)
+                fib_title = re.sub(r"<<<<\d+>>>>", "[IMG]", fib_title)
+                question.title = fib_title
                 list_of_text = replaced_answers.split("_______")
                 if answer_at_start:
                     list_of_text.pop(0)
-
-                # TODO: populate FIB models below
 
                 order = 1
                 while len(list_of_text) + len(list_of_answers) > 0:
@@ -413,6 +413,20 @@ def parse_question(questionlibrary, question):
                             pass
                     else:
                         try:
+                            # fib_list.append(list_of_text.pop(0))
+                            text_found = list_of_text.pop(0)
+                            fib_object = Fib.objects.create(question=question)
+                            fib_object.order = order
+                            fib_object.text = text_found
+                            fib_object.type = "fibquestion"
+                            fib_object.size = None
+                            fib_object.weight = None
+                            order += 1
+                            fib_object.save()
+                        except:
+                            pass
+
+                        try:
                             # fib_list.append(list_of_answers.pop(0))
                             answer_found = list_of_answers.pop(0)
                             fib_object = Fib.objects.create(question=question)
@@ -426,19 +440,6 @@ def parse_question(questionlibrary, question):
                         except:
                             pass
 
-                        try:
-                            # fib_list.append(list_of_text.pop(0))
-                            text_found = list_of_text.pop(0)
-                            fib_object = Fib.objects.create(question=question)
-                            fib_object.order = order
-                            fib_object.text = text_found
-                            fib_object.type = "fibquestion"
-                            fib_object.size = None
-                            fib_object.weight = None
-                            order += 1
-                            fib_object.save()
-                        except:
-                            pass
 
                 question.questiontype = 'FIB'
                 question.save()
